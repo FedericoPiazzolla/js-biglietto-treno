12b6c7ef993a6bc7dea1a3af493f635e88d81544
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-18 16:50:33 +0200

    add: calculated price with 2 decimals

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const userAge = parseInt(prompt("inserire la sua età"));
 console.log(userKm, userAge);
 
 // Logica del programma
+
+// Calcolo costo biglietto
+const ticketPriceKm = 0.21;
+const ticketPriceTotal = ticketPriceKm * userKm;
+const ticketPriceRounded = Math.round((ticketPriceTotal + Number.EPSILON ) * 100) / 100;
+console.log(ticketPriceRounded)
